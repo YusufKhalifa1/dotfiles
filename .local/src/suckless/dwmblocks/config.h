59fc64111e559fdf18b488fe1f61cb5e07a9b91b
yusuf <yusufkhalifa5151@gmail.com>
2023-11-21 13:04:40 +0200

    changed dwmblocks audio command

--- .local/src/suckless/dwmblocks/config.h
+++ .local/src/suckless/dwmblocks/config.h
@@ -1,9 +1,9 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/		/*Update Signal*/
-	{"Vol: ", "pactl list sinks | grep 'Sink #240' -A 10 | grep 'Volume:' | awk '{print $5}'",  	1,		10},
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-	{"", "date '+%b %d %a %H:%M'",					5,		0},
+	{"Vol: ", 	"pamixer --get-volume",  	1,						10},
+	{"Mem:", 	"free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
+	{"", 		"date '+%b %d %a %H:%M'",	5,						0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
